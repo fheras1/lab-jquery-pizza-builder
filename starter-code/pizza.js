ebe7f7280085894007306635538685a3f02fb5d6
Felix Heras <heras.felix@gmail.com>
2017-11-17 13:41:27 +0100

    feat: Iterations from one to three

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,35 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+
+  function toogleIngredientsBtn(btnClassName, ingredientClass) {
+    $(btnClassName).click(function() {
+      $(this).toggleClass("active");
+      $(ingredientClass).toggle();
+    });
+  }
+
+  function toogleSauceCrustBtn(btnClassName, permanentClass, addedClass) {
+    $(btnClassName).click(function() {
+      $(this).toggleClass("active");
+      $(permanentClass).toggleClass(addedClass);
+    });
+  }
+
+  function setDefaultSauceCrust(permanentClass, addedClass) {
+    $(permanentClass).removeClass(addedClass);
+  }
+
+  setDefaultSauceCrust(".sauce", "sauce-white");
+  setDefaultSauceCrust(".crust", "crust-gluten-free");
+
+  setDefaultSauceCrust(".btn-sauce", "active");
+  setDefaultSauceCrust(".btn-crust", "active");
+
+  toogleIngredientsBtn(".btn-pepperonni", ".pep");
+  toogleIngredientsBtn(".btn-mushrooms", ".mushroom");
+  toogleIngredientsBtn(".btn-green-peppers", ".green-pepper");
+  toogleSauceCrustBtn(".btn-sauce", ".sauce", "sauce-white");
+  toogleSauceCrustBtn(".btn-crust", ".crust", "crust-gluten-free");
+
+
+});
